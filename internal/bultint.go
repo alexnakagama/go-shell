473179e1c61cb016ef6b78dfd4f38c5ce4ffe560
alex <alexnakagama4@gmail.com>
2026-05-06 11:53:38 -0300

    added function to execute cd in builtin commands

--- internal/bultint.go
+++ internal/bultint.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -12,4 +13,16 @@ func ExecuteExit(args []string) {
 		}
 	}
 	os.Exit(0)
+}
+
+func ExecuteCd(args []string) {
+	if len(args) == 0 {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			fmt.Println(err)
+		}
+		os.Chdir(home)
+		return
+	}
+	os.Chdir(args[0])
 }
