f51d461800ff99780e67ea64c7383865ca24fab0
alex <alexnakagama4@gmail.com>
2026-05-06 21:10:43 -0300

    added function to start the shell

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
+import "github.com/alexnakagama/go-shell/internal"
 
 func main() {
-	fmt.Println("Hello world!")
+	internal.StartShell()
 }
--- internal/shell.go
+++ internal/shell.go
@@ -1,3 +1,34 @@
 package internal
 
 // contains the core shell logic and REPL loop, which reads user input, parses it, and executes commands
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func StartShell() {
+	for {
+		print("> ")
+
+		var input string
+		scanner := bufio.NewScanner(os.Stdin)
+		if scanner.Scan() {
+			input = scanner.Text()
+		} else if err := scanner.Err(); err != nil {
+			fmt.Println("Error reading input:", err)
+			continue
+		}
+
+		cmd := ParseInput(input)
+
+		if err := ExecuteCommand(cmd); err != nil {
+			fmt.Println("Error executing command:", err)
+		}
+
+		if cmd.Name == "exit" {
+			break
+		}
+	}
+}
